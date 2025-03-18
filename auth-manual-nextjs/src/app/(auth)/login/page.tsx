853777bb37b69d0ebb5243e9d75d2b2ae59cc1ab
nivaldoandrade <nivaldoandradef@gmail.com>
2025-03-18 13:53:04 -0300

    refactor: reorganize authentication pages and layout
    - Moved login and signup pages to the (auth) route group for better structure
    - Introduced a AuthLayout component to centralize the auth page layout
    - Simplified social login options in the login page

---
+++ auth-manual-nextjs/src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "@/components/login-form"
+
+export default function LoginPage() {
+  return (
+    <div className="w-full max-w-sm md:max-w-3xl">
+      <LoginForm />
+    </div>
+  )
+}

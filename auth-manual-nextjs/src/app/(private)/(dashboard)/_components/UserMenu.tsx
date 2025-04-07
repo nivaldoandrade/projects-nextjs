43eafd4a43d2f5e3da844d589994d19a3d50e5c1
nivaldoandrade <nivaldoandradef@gmail.com>
2025-04-07 18:34:39 -0300

    feat: add loading feedback on user sign-out flow
    - Implemented a loading spinner overlay while sign-out request is processing
    - Handled sign-out errors with toast notification
    - Added delay on sign-out API route for visial feedback testing

--- auth-manual-nextjs/src/app/(private)/(dashboard)/_components/UserMenu.tsx
+++ auth-manual-nextjs/src/app/(private)/(dashboard)/_components/UserMenu.tsx
@@ -2,41 +2,61 @@
 
 import { Button } from "@/components/ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger, DropdownMenuItem } from "@/components/ui/dropdown-menu";
+import { Spinner } from "@/components/ui/spinner";
 import { useAuth } from "@/hooks/useAuth";
 import axios from "axios";
 import { CircleUser } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { toast } from "sonner";
 
 export function UserMenu() {
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
   const { user } = useAuth();
 
   async function handleSignout() {
-    router.replace('/login');
-    await axios.post('/api/auth/sign-out');
+    try {
+      setIsLoading(true);
+      await axios.post('/api/auth/sign-out');
+      router.replace('/login');
+    } catch {
+      toast.error("Falha ao sair. Por favor, tente novamente");
+      setIsLoading(false);
+    }
   };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="secondary" size="icon" className="rounded-full">
-          <CircleUser className="h-5 w-5" />
-          <span className="sr-only">Toggle user menu</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuLabel>
-          <strong className="block">
-            {user.firstName} {user.lastName}
-          </strong>
-          <small className="text-muted-foreground">{user.email}</small>
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Settings</DropdownMenuItem>
-        <DropdownMenuItem>Support</DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem onSelect={handleSignout}>Logout</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="secondary" size="icon" className="rounded-full">
+            <CircleUser className="h-5 w-5" />
+            <span className="sr-only">Toggle user menu</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>
+            <strong className="block">
+              {user.firstName} {user.lastName}
+            </strong>
+            <small className="text-muted-foreground">{user.email}</small>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Settings</DropdownMenuItem>
+          <DropdownMenuItem>Support</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onSelect={handleSignout}>Logout</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      {
+        isLoading && (
+          <div className="fixed inset-0 bg-background z-100 grid place-items-center">
+            <Spinner size="large" />
+          </div>
+        )
+      }
+    </>
   );
 }
--- auth-manual-nextjs/src/app/api/auth/sign-out/route.ts
+++ auth-manual-nextjs/src/app/api/auth/sign-out/route.ts
@@ -1,9 +1,11 @@
 import { NextResponse } from "next/server"
 
 export async function POST() {
+  await new Promise(resolver => setTimeout(resolver, 400));
+
   const response = new NextResponse(null, {status: 204});
 
   response.cookies.delete('accessToken');
 
-  return response
+  return response;
 }
